5d7901ecf8c82ed380b36e0212c70e11fecb267b
Ligil V James <ligil@Ligils-MacBook-Air.local>
2022-05-26 12:46:38 +0530

    initial setup package

--- App.js
+++ App.js
@@ -7,7 +7,6 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -17,96 +16,38 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import FlashMessage from 'react-native-flash-message';
+import Icon from './src/components/Icon';
+import {NavigationContainer} from '@react-navigation/native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+ 
 
-const App: () => Node = () => {
+const App=() => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView >
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Text>iwenf</Text>
+      <NavigationContainer>
+
+      {/* <Icon
+                  from={'antDesign'}
+                  name="caretup"
+                  size={15}
+                  color="green"
+                  style={{paddingHorizontal: 10}}
+                /> */}
+      <FlashMessage position="top" />
+      </NavigationContainer>
+ 
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+
 
 export default App;
---
+++ src/components/Icon.js
@@ -0,0 +1,49 @@
+// import Icon from 'react-native-vector-icons/dist/AntDesign';
+import {View, Text} from 'react-native';
+import React from 'react';
+
+import Material from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+// import FontAwesome5Brands from 'react-native-vector-icons/FontAwesome5Brands';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Foundation from 'react-native-vector-icons/Foundation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Octicons from 'react-native-vector-icons/Octicons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import Zocial from 'react-native-vector-icons/Zocial';
+
+const Icon = ({from, ...rest}) => {
+  // console.log({...rest});
+  switch (from) {
+    case 'fontAwesome':
+      // size={size} color="#900
+      return <FontAwesome {...rest} />;
+    case 'material':
+      return <Material {...rest} />;
+    case 'antDesign':
+      return <AntDesign {...rest} />;
+    case 'entypo':
+      return <Entypo {...rest} />;
+
+    case 'evilIcons':
+      return <EvilIcons {...rest} />;
+    case 'feather':
+      return <Feather {...rest} />;
+    case 'fontisto':
+      return <Fontisto {...rest} />;
+    case 'ionicons':
+      return <Ionicons {...rest} />;
+    case 'materialIcons':
+      return <MaterialIcons {...rest} />;
+    default:
+      return <FontAwesome name={'question'} />;
+  }
+};
+
+export default Icon;
